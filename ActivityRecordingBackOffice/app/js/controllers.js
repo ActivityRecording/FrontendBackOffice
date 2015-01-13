a1ca41b0e94309c8f31320790619833614581626
walls2 <stefan@walle.ch>
2015-01-13 13:02:15 +0100

    BackOffice: Insert Update and Delete of Timeperiods

--- ActivityRecordingBackOffice/app/js/controllers.js
+++ ActivityRecordingBackOffice/app/js/controllers.js
@@ -87,7 +87,7 @@ function ActivitiesCtrl($scope, $routeParams, $location, Approval, Activity, Cas
     $scope.supplierName = '';
     $scope.tarmedDescription = '';
     $scope.duration = '';
-    
+    $scope.filterText = '';
 
     //Speichern einer bearbeiteten oder neuen Leistung
     $scope.saveActivity = function () {
@@ -188,39 +188,53 @@ function ActivitiesCtrl($scope, $routeParams, $location, Approval, Activity, Cas
         $scope.createActivity = true;
         $scope.activitiesForm.$setPristine();
     };
-
-
+    
+    $scope.isGenerated = function(tarmedActivityId){
+        return tarmedActivityId === '00.0010' ||
+               tarmedActivityId === '00.0020' ||
+               tarmedActivityId === '00.0030' ||
+               tarmedActivityId === '00.2285';
+    }
+    
+    $scope.search = function (item){
+        if (!$scope.isGenerated(item.id) && (item.id.indexOf($scope.filterText) != -1 || item.description.indexOf($scope.filterText) != -1)) {
+            return true;
+        }
+        return false;
+    };
 
     //TimePeriod Funktionen
     $scope.savePeriod = function () {
-        var isNew = $scope.currentTimePeriod.timePeriodId == null;
         var container = new TimePeriod({'timePeriodId': null, 'type': 'TREATMENT'});
-        container.employeeId = $scope.currentTimePeriod.supplier.employeeID;
+        container.employeeId = $scope.currentTimePeriod.employeeId;
         container.treatmentNumber = $scope.fid;
         container.startTime = $scope.currentTimePeriod.startTime;
         container.endTime = $scope.currentTimePeriod.endTime;
-        if (isNew) {
+        if ($scope.createPeriod) {
             container.$save().then(function () {
                 $scope.periods = TimePeriod.query({fid: $scope.fid});
                 $scope.activities = Approval.query({fid: $scope.fid});
                 $scope.caseTime = CaseTime.get({fid: $scope.fid});
+                $scope.cancel();
+                $scope.timePeriodForm.$setPristine();
             });
         } else {
+            container.timePeriodId = $scope.currentTimePeriod.timePeriodId;
             container.$update().then(function () {
                 $scope.periods = TimePeriod.query({fid: $scope.fid});
                 $scope.activities = Approval.query({fid: $scope.fid});
                 $scope.caseTime = CaseTime.get({fid: $scope.fid});
+                $scope.cancel();
+                $scope.timePeriodForm.$setPristine();
             });
         }
-        $scope.cancel();
-        $scope.timePeriodForm.$setPristine();
     };
 
     $scope.editPeriod = function (period) {
         $scope.createPeriod = false;
         $scope.currentTimePeriod = period;
         $scope.supplierName = period.employeeId+': '+period.supplierLastname+' '+period.supplierFirstname;
-        $scope.duration = getTimeDiff(period.startTime, period.endTime );
+        $scope.duration = $scope.getDuration(period.startTime, period.endTime );
     };
 
     $scope.cancelPeriod = function () {
@@ -232,17 +246,24 @@ function ActivitiesCtrl($scope, $routeParams, $location, Approval, Activity, Cas
         var index = $scope.periods.indexOf(item);
         if (index === -1)
             return;
-        TimePeriod.delete({fid: item.timePeriodId},
-        function () {
-            $scope.periods.splice(index, 1);
-            $scope.periods = TimePeriod.query({fid: $scope.fid});
-            $scope.activities = Approval.query({fid: $scope.fid});
-            $scope.caseTime = CaseTime.get({fid: $scope.fid});
-        }
+        TimePeriod.delete(
+                {fid: item.timePeriodId},
+                function () {
+                    $scope.periods.splice(index, 1);
+                    $scope.periods = TimePeriod.query({fid: $scope.fid});
+                    $scope.activities = Approval.query({fid: $scope.fid});
+                    $scope.caseTime = CaseTime.get({fid: $scope.fid});
+                },
+                function (err){
+                    alert("Im Service TimePeriod.delete ist ein Fehler aufgetreten: " + err);
+                }
         );
     };
 
-
+    $scope.getDuration = function(start, end){
+        var duration = getTimeDiff( start, end );
+        return duration.hours + ' Std ' + duration.minutes + ' Min ' + duration.seconds + ' Sek';
+    }
 
     $scope.back = function () {
         $location.path('/view.cases/');
@@ -266,9 +287,18 @@ function ApprovalCtrl($scope, $routeParams, $location, Approval, CaseTime, TimeP
 
     $scope.approve = function () {
         var approval = new Approval({fid: $scope.fid});
-        approval.$save();
-        $location.path('/view.cases');
+        approval.$save().then(
+            function(){
+                $location.path('/view.cases');
+            }       
+        );
+        
     };
+    
+    $scope.getDuration = function(start, end){
+        var duration = getTimeDiff( start, end );
+        return duration.hours + ' Std ' + duration.minutes + ' Min ' + duration.seconds + ' Sek';
+    }
 }
 ;
 
@@ -305,14 +335,14 @@ function SuppliersCtrl($scope, Supplier) {
 };
 
 
-function getTimeDiff( t0, t1 )
-{
-    if (t0 < t1) { 
-        var milisec_diff = t1 - t0;
-    }else{
-        var milisec_diff = t0 - t1;
-    }
-    var days = Math.floor(milisec_diff / 1000 / 60 / (60 * 24));
-    var date_diff = new Date( milisec_diff );
-    return days + " Days "+ date_diff.getHours() + " Hours " + date_diff.getMinutes() + " Minutes " + date_diff.getSeconds() + " Seconds";
+
+function getTimeDiff( start, end ){
+    var startTime = Date.parse(start);
+    var endTime = Date.parse(end);
+    var seconds = (endTime - startTime)/1000;
+    var hours = Math.floor(seconds / (60 * 60));
+    seconds = seconds - (hours * 60 *60);
+    var minutes = Math.floor(seconds / 60);
+    seconds = seconds - (minutes * 60);
+    return {'hours': hours, 'minutes': minutes, 'seconds': seconds};
 }
